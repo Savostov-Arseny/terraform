d1087ca535a65e075e4a0e56fb4d0baebb8aa7ba
Savostov-Arseny <savostov.arseny@gmail.com>
2020-01-18 16:44:07 +0400

    Refactoring variable calls

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "Swarm_master" {
   count                  = 1
-  ami                    = "${var.ami}"
-  instance_type          = "${var.instance_type}"
-  key_name               = "${var.key_name}"
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
   user_data              = file("${var.bootstrap_swarm_path}")
   vpc_security_group_ids = ["${aws_security_group.swarm.id}"]
   tags = {
@@ -12,10 +12,10 @@ resource "aws_instance" "Swarm_master" {
 
 resource "aws_instance" "Swarm_agent" {
   count                  = 8
-  ami                    = "${var.ami}"
-  instance_type          = "${var.instance_type}"
-  key_name               = "${var.key_name}"
-  user_data              = file("${var.bootstrap_swarm_path}")
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  user_data              = file(var.bootstrap_swarm_path)
   vpc_security_group_ids = ["${aws_security_group.swarm.id}"]
   tags = {
     Name = "Swarm_agent_${count.index}"
@@ -33,9 +33,9 @@ resource "template_file" "user_data_management" {
 
 resource "aws_instance" "Management_master" {
   count                  = 1
-  ami                    = "${var.ami}"
-  instance_type          = "${var.instance_type}"
-  key_name               = "${var.key_name}"
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
   user_data              = "${template_file.user_data_management.rendered}"
   vpc_security_group_ids = ["${aws_security_group.swarm.id}"]
   tags = {
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.aws_region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.aws_region
   version    = "2.29"
 }
 
