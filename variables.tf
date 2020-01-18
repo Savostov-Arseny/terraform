f6b8231e04ab263e323e8d9cfca59a6be9db1f66
Savostov-Arseny <savostov.arseny@gmail.com>
2020-01-18 22:00:22 +0400

    Update variables.tf for convenient using .tfvars

--- variables.tf
+++ variables.tf
@@ -1,24 +1,18 @@
 #Main variables
 variable "aws_region" {
-  default = "eu-north-1"
+  description = "AWS region"
 }
-#ec2 variables
+variable "access_key" {
+}
+variable "secret_key" {
+}
 
-variable "ami" {
-  description = "Amazon Ubuntu Server 18.04 LTS (HVM), SSD Volume Type (64-bit x86) AMI"
-  default     = "ami-ada823d3"
-}
+#ec2 variables
 variable "instance_type" {
   description = "Instance type"
-  default     = "t3.micro"
-}
-variable "key_path" {
-  description = "SSH Public Key path"
-  default     = "" # type key path here
 }
 variable "key_name" {
   description = "Desired name of Keypair..."
-  default     = "" #type existing key name here
 }
 variable "bootstrap_swarm_path" {
   description = "Script to install Docker Engine and docker-compose"
