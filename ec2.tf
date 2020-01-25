0b77204b618f6450a2be6eea6539b37c17ddf26e
Savostov-Arseny <savostov.arseny@gmail.com>
2020-01-26 02:54:01 +0400

    Add Jenkins_master configuration

--- ec2.tf
+++ ec2.tf
@@ -10,6 +10,7 @@ data "aws_ami" "ubuntu_ami" {
   owners = ["099720109477"] #Canonical
 }
 
+## Docker swarm
 resource "aws_instance" "Swarm_master" {
   count                  = 1
   ami                    = "${data.aws_ami.ubuntu_ami.id}"
@@ -40,10 +41,25 @@ resource "aws_instance" "Ansible_master" {
   ami                    = "${data.aws_ami.ubuntu_ami.id}"
   instance_type          = var.instance_type
   key_name               = var.key_name
-  user_data              = templatefile(var.bootstrap_ansible_path, { private_key = file(var.private_key_path) })
+  user_data              = templatefile(var.bootstrap_ansible_path, { public_key = file(var.public_key_path), private_key = file(var.private_key_path) })
   vpc_security_group_ids = ["${aws_security_group.swarm.id}"]
   iam_instance_profile   = "${aws_iam_instance_profile.ec2_read_access_profile.name}"
   tags = {
     Name = "Ansible_master"
+  }
+}
+
+## Jenkins_master
+resource "aws_instance" "Jenkins_master" {
+  count         = 1
+  ami           = "${data.aws_ami.ubuntu_ami.id}"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  user_data = templatefile(var.bootstrap_jenkins_path,
+    { public_key = file(var.public_key_path), private_key = file(var.private_key_path), juser = var.juser, jpass = var.jpass,
+  dockeruser = var.dockeruser, dockerpassword = var.dockerpassword, ansible_host = "${aws_instance.Ansible_master[0].private_ip}" })
+  vpc_security_group_ids = ["${aws_security_group.swarm.id}"]
+  tags = {
+    Name = "Jenkins_master"
   }
 }
--- variables.tf
+++ variables.tf
@@ -24,10 +24,29 @@ variable "bootstrap_ansible_path" {
   default     = "Bootstrap_scripts/user_data_ansible.tmpl"
 }
 
+variable "bootstrap_jenkins_path" {
+  description = "Script to install Ansible & configurations"
+  default     = "Bootstrap_scripts/user_data_jenkins.tmpl"
+}
+
 # RSA keys
 variable "public_key_path" {
   description = "path to public_key"
 }
 variable "private_key_path" {
   description = "path to private_key"
+}
+
+#Jenkins_master variables
+variable "juser" {
+  default = "Jenkins default user login"
+}
+variable "jpass" {
+  default = "Jenkins default user password"
+}
+variable "dockeruser" {
+  default = "Dockerhub user"
+}
+variable "dockerpassword" {
+  default = "Dockerhub password"
 }
